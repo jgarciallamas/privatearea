cb149f38fcc776c0f94fb42838004c9c76779881
Jose <jgarciallamas@yahoo.es>
2022-07-30 16:47:49 +0200

    Create homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-lg">Index page</h1>
+      <Head>
+        <title>Private Area</title>
+        <meta name="description" content="Private Area" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="text-center ">
+        <h1 className=" mt-20 font-extrabold text-2xl">Private Area</h1>
+
+        <p className="mt-10">Join the private area to have access to</p>
+
+        <ol className="mt-10 list-inside list-decimal">
+          <li>The lyrics book in PDF</li>
+          <li>Exclusive 30% discount on the albums</li>
+          <li>Exclusive access to preorders</li>
+        </ol>
+
+        <p className="mt-10">Just $5/m</p>
+
+        <div className="mt-10">
+          <a className="bg-black text-white px-5 py-2" href="#">
+            Become a supporter
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
